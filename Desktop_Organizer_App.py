5719c0b92c9d83591429af02480e093a0c4d115b
TikTumanyan <tigran.r.tumanyan@gmail.com>
2025-09-22 00:31:42 +0000

    Add files via upload

---
+++ Desktop_Organizer_App.py
@@ -0,0 +1,31 @@
+import os
+import shutil
+
+types = {
+    'Pictures New': ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff'],
+    'Videos New': ['.mp4', '.avi', '.mkv', '.mov', '.flv', '.wmv'],
+    'Documents New': ['.pdf', '.docx', '.txt', '.xls', '.ppt', '.md'],
+    'All Other': []
+}
+
+desktop = os.path.join(os.path.expanduser('~'), 'Desktop')
+
+for folder in types:
+    os.makedirs(os.path.join(desktop, folder), exist_ok=True)
+
+def organizing():
+    for file in os.listdir(desktop):
+        file_path = os.path.join(desktop, file)
+        if os.path.isdir(file_path): continue
+        ext = os.path.splitext(file)[1].lower()
+        moved = False
+        for folder, extensions in types.items():
+            if ext in extensions:
+                shutil.move(file_path, os.path.join(desktop, folder, file))
+                moved = True
+                break
+        if not moved:
+            shutil.move(file_path, os.path.join(desktop, 'Other', file))
+
+if __name__ == "__main__":
+    organizing()
